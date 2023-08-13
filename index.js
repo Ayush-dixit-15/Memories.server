96fe1b4f6c6b07f3f32c8774ede28398679efcff
Ayush-dixit-15 <ayushdixit632@gmail.com>
2023-08-13 09:23:47 +0530

    backend

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+app.use('/posts', postRoutes);
+app.get('/',(req,res)=>{
+  res.send('Hello and welcome to memories');
+})
+const connect1 = process.env.CONNECT; 
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(connect1, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Connected to mongo and Server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
